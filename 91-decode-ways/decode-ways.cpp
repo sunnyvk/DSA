6fff94af51a893f23dcee8a769767262189d3ff1
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-04 17:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (56.18%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -3,19 +3,24 @@ public:
     int numDecodings(string s) {
         int n = s.length();
         vector<int> dp(n+1, -1);
-        dp[n] = 1;
+       int next1 = 1;  
+        int next2 = 0;  
+        int curr = 0;
         for (int i = n - 1; i >= 0; i--) {
                   if (s[i] == '0'){
-                    dp[i] = 0;  
+                    curr = 0; 
+                      next2 = next1;
+                next1 = curr; 
                     continue;
                   }
-            int result = dp[i + 1];
+           curr= next1;
              if (i + 1 < n) {
                 if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))
-                    result += dp[i + 2];
+                    curr += next2;
             }
-            dp[i] = result;
+            next2=next1;
+            next1 = curr;
         }
-        return dp[0];
+        return curr;
     }
 };
