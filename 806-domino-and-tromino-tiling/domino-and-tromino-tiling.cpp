b3cd5b9b572f65e26d0e10434dc4af20387723ec
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-06 00:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.47%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int MOD=1e9+7;
+    int numTilings(int n) {
+        if(n==1 || n==2)
+        return n;
+        vector<int> dp(n+1,-1);
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int i=4;i<=n;i++){
+            dp[i]=(2*dp[i-1]%MOD+dp[i-3]%MOD)%MOD;
+        }
+        return dp[n];
+    }
+};
