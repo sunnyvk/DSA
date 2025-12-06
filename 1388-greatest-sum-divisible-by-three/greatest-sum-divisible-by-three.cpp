ee8d49b2a0ac7d32b25eca4d63867b03717e240f
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-06 20:46:25 +0530

    Time: 44 ms (14.48%) | Memory: 42.6 MB (11.96%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -10,7 +10,7 @@ public:
         dp[n][2] = INT_MIN;
         for (int i = n - 1; i >= 0; i--) {
             for (int rem = 0; rem <= 2; rem++) {
-                int take = (dp[i + 1][(rem + nums[i]) % 3] == INT_MIN)
+                int take = dp[i + 1][(rem + nums[i]) % 3] == INT_MIN
                                ? INT_MIN
                                : (nums[i] + dp[i + 1][(rem + nums[i]) % 3]);
                 int skip = dp[i + 1][rem];
