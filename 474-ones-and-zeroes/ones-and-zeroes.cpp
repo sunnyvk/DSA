aa9379fd20d3a4e75c6f0ee5143e98a58d0db0bb
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-08 12:12:44 +0530

    Time: 151 ms (36.82%) | Memory: 106.9 MB (9.04%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    int solve(int i, int m, int n, vector<pair<int, int>>& count,vector<vector<vector<int>>> &dp) {
+        if (i < 0)
+            return 0;
+        int take = 0;
+        if(dp[i][m][n]!=-1) return dp[i][m][n];
+        if (count[i].first <= m && count[i].second <= n) {
+             take = 1 + solve(i - 1, m - count[i].first, n - count[i].second,
+                                 count,dp);
+        }
+        int skip = solve(i - 1, m, n, count,dp);
+        return dp[i][m][n]=max(take, skip);
+    }
+
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int N = strs.size();
+        vector<pair<int, int>> count(N);
+        vector<vector<vector<int>>> dp(N+1,vector<vector<int>> (m+1,vector<int>(n+1,-1) ));
+
+        for (int i = 0; i < N; i++) {
+            int countofzeroes = 0;
+            int countofones = 0;
+            for (char ch : strs[i]) {
+                if (ch == '0') {
+                    countofzeroes++;
+                } else {
+                    countofones++;
+                }
+            }
+            count[i] = {countofzeroes, countofones};
+        }
+
+        return solve(N - 1, m, n, count,dp);
+    }
+};
