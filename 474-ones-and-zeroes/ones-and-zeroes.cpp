1ac9dfd777b33942c69c7a167bf742827ae2557e
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-08 12:42:50 +0530

    Time: 226 ms (10.70%) | Memory: 106.7 MB (13.31%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,18 +1,4 @@
 class Solution {
-    // int solve(int i, int m, int n, vector<pair<int, int>>&
-    // count,vector<vector<vector<int>>> &dp) {
-    //     if (i < 0 || (m==0 && n==0))
-    //         return 0;
-    //     int take = 0;
-    //     if(dp[i][m][n]!=-1) return dp[i][m][n];
-    //     if (count[i].first <= m && count[i].second <= n) {
-    //          take = 1 + solve(i - 1, m - count[i].first, n - count[i].second,
-    //                              count,dp);
-    //     }
-    //     int skip = solve(i - 1, m, n, count,dp);
-    //     return dp[i][m][n]=max(take, skip);
-    // }
-
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int N = strs.size();
