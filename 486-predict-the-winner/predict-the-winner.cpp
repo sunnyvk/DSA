af3f7a95a77b2251fd08074bc5975cc3116f88e2
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-13 13:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (95.58%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-int n;
- int t[23][23];
-    int solve(vector<int>& nums, int l, int r) {
-        
-        if(l > r)
-            return 0;
+   int n;
+    int t[23][23];
+    
+   
+    int maxDiff(vector<int>& nums, int l, int r) {
         
         if(l == r)
             return nums[l];
@@ -13,27 +12,18 @@ int n;
         if(t[l][r] != -1)
             return t[l][r];
         
-        int take_left  = nums[l] + min(solve(nums, l+2, r), solve(nums, l+1, r-1));
-        
-        int take_right = nums[r] + min(solve(nums, l, r-2), solve(nums, l+1, r-1));
-        
+        int take_left  = nums[l] - maxDiff(nums, l+1, r);
+        int take_right = nums[r] - maxDiff(nums, l, r-1);
         
         return t[l][r] = max(take_left, take_right);
-        
     }
     
     bool predictTheWinner(vector<int>& nums) {
-        
-        memset(t, -1, sizeof(t));
-        
         n = nums.size();
-        
-        
-        int total = accumulate(begin(nums), end(nums), 0);
-        
-        int player1 = solve(nums, 0, n-1);
-        int player2 = total - player1;
-        
-        return player1 >= player2;
+        memset(t, -1, sizeof(t));
+        return maxDiff(nums, 0, n-1) >= 0;
+            
     }
+        
+     
 };
