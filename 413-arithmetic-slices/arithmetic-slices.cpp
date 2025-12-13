f0118a7bc5883ce48b9fe6c9defc3458ec1b1b27
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-13 11:26:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (12.05%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-int solve(int i,vector<int>& nums,vector<int> &dp){
-    if(i<2) return 0;
-    int cnt=0;
-    if(dp[i]!=-1) return dp[i];
-    if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
-        cnt=1+solve(i-1,nums,dp);
-    }
-    return dp[i]=cnt;
-}
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n=nums.size();
         int ans=0;
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
+        if(n<3) return 0;
         for(int i=2;i<n;i++){
-                ans+=solve(i,nums,dp);
+               if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+        dp[i]=1+dp[i-1];
+    }
+    ans+=dp[i];
         }
         return ans;
     }
