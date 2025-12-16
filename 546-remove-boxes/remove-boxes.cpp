9b15270c940c74885651b773b44d9a62baa6b429
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2025-12-17 00:26:45 +0530

    Time: 656 ms (5.13%) | Memory: 14.8 MB (56.19%) - LeetSync

--- 546-remove-boxes/remove-boxes.cpp
+++ 546-remove-boxes/remove-boxes.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int dp[101][101][101];
-    int solve(int l, int r, int k, vector<int>& boxes) {
-        if (l > r) return 0;
-        if (dp[l][r][k] != -1)
-            return dp[l][r][k];
-        int ans = (k + 1) * (k + 1) + solve(l + 1, r, 0, boxes);
+  int dp[101][101][102];
+    int removeBoxes(vector<int>& boxes) {
+        int n = boxes.size();
+       
+        memset(dp, 0, sizeof(dp));
 
- 
-        for (int i = l + 1; i <= r; i++) {
-            if (boxes[i] == boxes[l]) {
-                ans = max(ans,
-                          solve(l + 1, i - 1, 0, boxes) +
-                          solve(i, r, k + 1, boxes));
+        // length = size of subarray
+        for (int len = 1; len <= n; len++) {
+            for (int l = 0; l + len - 1 < n; l++) {
+                int r = l + len - 1;
+
+                for (int k = 0; k <= n; k++) {
+
+                    // Option 1: remove boxes[l] now
+                    dp[l][r][k] = (k + 1) * (k + 1);
+                    if (l + 1 <= r)
+                        dp[l][r][k] += dp[l + 1][r][0];
+
+                    // Option 2: merge with same color later
+                    for (int i = l + 1; i <= r; i++) {
+                        if (boxes[i] == boxes[l]) {
+                            dp[l][r][k] = max(
+                                dp[l][r][k],
+                                dp[l + 1][i - 1][0] + dp[i][r][k + 1]
+                            );
+                        }
+                    }
+                }
             }
         }
-        return dp[l][r][k] = ans;
-    }
 
-    int removeBoxes(vector<int>& boxes) {
-          memset(dp, -1, sizeof(dp));
-        return solve(0, boxes.size() - 1, 0, boxes);
+        return dp[0][n - 1][0];
     }
 };
+
