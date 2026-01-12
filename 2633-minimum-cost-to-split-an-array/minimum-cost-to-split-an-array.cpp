58982c8c1a0651d95ea968ab6ecfbb497c2ed520
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-01-12 19:54:32 +0530

    Time: 1186 ms (37.62%) | Memory: 315.8 MB (58.72%) - LeetSync

--- 2633-minimum-cost-to-split-an-array/minimum-cost-to-split-an-array.cpp
+++ 2633-minimum-cost-to-split-an-array/minimum-cost-to-split-an-array.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-int n;
-int solve(int i,vector<int> &nums,int k, vector<int> &dp){
-    if(i==n){
-        return 0;
-    }
-    if(dp[i]!=-1) return dp[i];
-    unordered_map<int,int> mpp;
-    int ans=INT_MAX;
-    int count=0;
-    for(int j=i;j<n;j++){
-        mpp[nums[j]]++;
-        if(mpp[nums[j]]==2){
-            count+=2;
-        }
-        if(mpp[nums[j]]>2){
-            count++;
-        }
-    ans=min(ans,k+solve(j+1,nums,k,dp)+count);
-        
-    }
-return dp[i]=ans;
-}
     int minCost(vector<int>& nums, int k) {
-         n=nums.size();
-         vector<int> dp(n + 1, -1);
-        return solve(0,nums,k,dp);
+        int n = nums.size();
+        vector<int> dp(n + 1, INT_MAX);
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            unordered_map<int, int> mpp;
+            int ans = INT_MAX;
+            int count = 0;
+            for (int j = i; j < n; j++) {
+                mpp[nums[j]]++;
+                if (mpp[nums[j]] == 2) {
+                    count += 2;
+                }
+                if (mpp[nums[j]] > 2) {
+                    count++;
+                }
+                dp[i] = min(dp[i], k + dp[j + 1] + count);
+            }
+        
+        }
+        return dp[0];
     }
 };
