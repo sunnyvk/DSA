91338cfb7389940248bbb17f2b1606e9f59deefc
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-01-20 11:48:56 +0530

    Time: 3 ms (7.03%) | Memory: 8.4 MB (8.56%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
         int t=0;
-        while(true){
+        for(t=1;t<=60;t++){
             long long val=(long long)num1-(long long)t*num2;
-            if(val<0){
-                return -1;
-            }
-            if(val<0) return -1;
+            if(val<0) continue;
             if(__builtin_popcountll(val)<=t && t<=val){
                 return t;
             }
-            t++;
-        } 
+        }
         return -1;
     }
 };
