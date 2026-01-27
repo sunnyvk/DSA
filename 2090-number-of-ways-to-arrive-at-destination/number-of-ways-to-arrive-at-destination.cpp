6d6895ff45817396db89a163bb86e75301a10636
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-01-27 21:13:40 +0530

    Time: 19 ms (12.85%) | Memory: 37.3 MB (46.81%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto it:roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
-        }
+const int MOD = 1e9 + 7;
+    using T = pair<long long, int>;
 
-        priority_queue<pair<long long,int>,
-        vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
+    int countPaths(int n, vector<vector<int>>& roads) {
+       vector<vector<pair<int,int>>> adj(n);
+       for(auto it:roads){
+        adj[it[0]].push_back({it[1],it[2]});
+        adj[it[1]].push_back({it[0],it[2]});
+       } 
+       priority_queue<T,vector<T>,greater<T>> pq;
+       vector<int> ways(n,0);
         vector<long long> dist(n,LLONG_MAX);
-        vector<int> ways(n,0);
         dist[0]=0;
         ways[0]=1;
-
         pq.push({0,0});
-        int mod=(int)(1e9+7);
         while(!pq.empty()){
             long long dis=pq.top().first;
             int node=pq.top().second;
             pq.pop();
             for(auto it:adj[node]){
                 int adjNode=it.first;
-                long long edW=it.second;
-                if(dis+edW < dist[adjNode]){
+                int edW=it.second;
+                if(dis+edW<dist[adjNode]){
                     dist[adjNode]=dis+edW;
                     pq.push({dis+edW,adjNode});
                     ways[adjNode]=ways[node];
-                }else if(dis+edW ==dist[adjNode]){
-                    ways[adjNode]=(ways[adjNode]+ways[node])%mod;
-
+                }else if(dis+edW==dist[adjNode]){
+                        ways[adjNode]=(ways[adjNode]+ways[node])%MOD;
                 }
             }
         }
-        return ways[n-1]%mod;
+
+      return ways[n-1]%MOD;
+       
+
     }
 };
