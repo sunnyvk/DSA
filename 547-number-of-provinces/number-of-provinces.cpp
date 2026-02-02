6db11d2f7727e689a0f0b7b9b1c24b000db5c57d
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-02 12:55:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (83.10%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,67 @@
-class Solution {
+class DisjointSet {
+    vector<int> rank, parent, size;
+
 public:
-    void dfs(int node, vector<vector<int>>& adjLs, vector<int>& vis) {
-        vis[node] = 1;
-        for (auto it : adjLs[node]) {
-            if (!vis[it]) {
-                dfs(it, adjLs, vis);
-            }
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int v = isConnected.size();
-        vector<vector<int>> adjLs(v);
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < v; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
-                }
-            }
+    int findUPar(int node) {
+        if (node == parent[node]) {
+            return node;
         }
-        vector<int> vis(v, 0);
-        int cnt = 0;
-        for (int i = 0; i < v; i++) {
-            if (!vis[i]) {
-                cnt++;
-                dfs(i, adjLs, vis);
-            }
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
         }
-        return cnt;
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else  {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
     }
 };
+    class Solution {
+    public:
+        int findCircleNum(vector<vector<int>>& isConnected) {
+            int v = isConnected.size();
+            DisjointSet ds(v);
+            for (int i = 0; i < v; i++) {
+                for (int j = 0; j < v; j++) {
+                    if(isConnected[i][j]==1){
+                    ds.unionBySize(i, j);
+                    }
+                }
+            }
+            int cnt = 0;
+            for (int i = 0; i < v; i++) {
+                if (ds.findUPar(i) == i)
+                    cnt++;
+            }
+            return cnt;
+        }
+    };
