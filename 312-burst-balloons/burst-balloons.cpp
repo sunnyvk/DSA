b87194c015571612d1b51a66e9bb954601eb6b7a
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-04 11:09:11 +0530

    Time: 149 ms (55.02%) | Memory: 10.7 MB (99.93%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -3,13 +3,11 @@ public:
 int dp[305][305];
 int solve(int i,int j,vector<int>& nums){
     if(i>j) return 0;
-    
+    if(dp[i][j]!=-1) return dp[i][j]; 
     int maxi=INT_MIN;
-    if (dp[i][j] != -1)
-            return dp[i][j];
     for(int ind=i;ind<=j;ind++){
-     int  coins=nums[i-1]*nums[ind]*nums[j+1]+solve(i,ind-1,nums)+solve(ind+1,j,nums);
-        maxi=max(maxi,coins);
+        int cost=nums[i-1]*nums[ind]*nums[j+1]+solve(i,ind-1,nums)+solve(ind+1,j,nums);
+        maxi=max(maxi,cost);
     }
     return dp[i][j]=maxi;
 }
@@ -17,7 +15,7 @@ int solve(int i,int j,vector<int>& nums){
         int n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        memset(dp, -1, sizeof(dp));
+        memset(dp,-1,sizeof(dp));
        return solve(1,n,nums);
     }
 };
