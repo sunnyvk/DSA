75a21d74b44327dc1b2e80ec508050d3864d9dbc
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-08 12:28:32 +0530

    Time: 20 ms (67.16%) | Memory: 12.9 MB (31.83%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     bool dfs(int side, int idx, int currSum,
-             vector<int>& matchsticks, vector<bool>& used, int target) {
+             vector<int>& sticks, vector<bool>& used, int target) {
 
-        // If 3 sides are completed, the 4th is automatically correct
-        if (side == 3) return true;
+        // If 3 sides are successfully formed,
+        // the 4th side is automatically correct
+        if (side == 3)
+            return true;
 
         // Current side completed → move to next side
-        if (currSum == target) {
-            return dfs(side + 1, 0, 0, matchsticks, used, target);
-        }
+        if (currSum == target)
+            return dfs(side + 1, 0, 0, sticks, used, target);
 
-        for (int i = idx; i < matchsticks.size(); i++) {
+        for (int i = idx; i < sticks.size(); i++) {
             if (used[i]) continue;
-            if (currSum + matchsticks[i] > target) continue;
+            if (currSum + sticks[i] > target) continue;
 
             used[i] = true;
-            if (dfs(side, i + 1, currSum + matchsticks[i],
-                    matchsticks, used, target))
+            if (dfs(side, i + 1, currSum + sticks[i],
+                    sticks, used, target))
                 return true;
             used[i] = false;
 
-            // pruning: if first stick fails, no need to try others at same level
+            // Pruning: if first stick doesn't work,
+            // no need to try others at same level
             if (currSum == 0) break;
         }
         return false;
@@ -38,3 +40,4 @@ public:
         return dfs(0, 0, 0, matchsticks, used, target);
     }
 };
+
