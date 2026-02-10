981b3249d3edc41b165d2ea66ec11c7aa48ef60a
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-11 01:44:39 +0530

    Time: 163 ms (8.14%) | Memory: 14.8 MB (65.31%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,7 +1,5 @@
 class Solution {
 public:
-    int t[101][101];
-    
     int countSteps(int ringIndex, int i, int n) {
         int dist       = abs(i - ringIndex);
         int wrapAround =  n - dist;
@@ -9,30 +7,32 @@ public:
         return min(dist, wrapAround);
     }
     
-    int solve(int ringIndex, int keyIndex, string& ring, string& key) {
+    int findRotateSteps(string ring, string key) {
+        int n = ring.length();
+        int m = key.length(); 
         
-        if (keyIndex == key.length()) {
-            return 0;
+        vector<vector<int>> t(n+1, vector<int>(m+1, INT_MAX));
+        //t[ringIndex][keyIndex] = minimum number of steps to get key[keyIndex] when the ring[ringIndex] is aligned with the "12:00" position.
+        
+        //If 
+        for(int ringIndex = 0; ringIndex < n; ringIndex++) {
+            t[ringIndex][m] = 0; //base case when all key chars are done (we reached index n)
         }
-        
-        if(t[ringIndex][keyIndex] != -1) {
-            return t[ringIndex][keyIndex];
-        }
-        
-        int result = INT_MAX;
-        for (int i = 0; i < ring.length(); i++) {
-            if (ring[i] == key[keyIndex]) {
-                int totalSteps = countSteps(ringIndex, i, ring.length()) + 1 +
-                                            solve(i, keyIndex + 1, ring, key);
-                result = min(result, totalSteps);
+    
+        for(int keyIndex = m-1; keyIndex >= 0; keyIndex--) {
+            for(int ringIndex = 0; ringIndex < n; ringIndex++) {
+                
+                 int result = INT_MAX;
+                 for (int i = 0; i < ring.length(); i++) {
+                    if (ring[i] == key[keyIndex]) {
+                        int totalSteps = countSteps(ringIndex, i, ring.length()) + 1 +
+                                                    t[i][keyIndex + 1];
+                        result = min(result, totalSteps);
+                    }
+                }
+                t[ringIndex][keyIndex] = result;
             }
         }
-        
-        return t[ringIndex][keyIndex] = result;
-    }
-    
-    int findRotateSteps(string ring, string key) {
-        memset(t, -1, sizeof(t));
-        return solve(0, 0, ring, key);
+        return t[0][0];
     }
 };
