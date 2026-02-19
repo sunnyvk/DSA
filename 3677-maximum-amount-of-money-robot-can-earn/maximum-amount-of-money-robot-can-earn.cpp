bb4407fcc175cdbda7cee4cf4d33108ca3aea680
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-19 16:02:34 +0530

    Time: 654 ms (37.50%) | Memory: 277.3 MB (14.00%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
-int dp[501][501][3];
-    int maximumAmount(vector<vector<int>>& coins) {
-        
-                     int n = coins.size();
-        int m = coins[0].size();
-         for (int i = 0; i < 501; i++)
-            for (int j = 0; j < 501; j++)
-                for (int k = 0; k < 3; k++)
-                    dp[i][j][k] = INT_MIN;
-
-         for (int neu = 0; neu <= 2; neu++) {
-            if (neu > 0 && coins[n-1][m-1] < 0)
-                dp[n-1][m-1][neu] = 0;
-            else
-                dp[n-1][m-1][neu] = coins[n-1][m-1];
+vector<vector<vector<int>>> dp;
+  int  solve(int i,int j,int neu,vector<vector<int>>& coins){
+        int n=coins.size();
+        int m=coins[0].size();
+        if(dp[i][j][neu]!=INT_MIN) return dp[i][j][neu];
+        if(i>=n || j>=m) return INT_MIN;
+        if(i==n-1 && j==m-1){
+            if(neu>0 && coins[i][j]<0) return 0;
+            else return coins[i][j];
         }
-          for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (i == n - 1 && j == m - 1) continue;
-                     for (int neu = 0; neu <= 2; neu++) {
-
-                    int best = INT_MIN;
-
-                   
-                    if (i + 1 < n)
-                        best = max(best, coins[i][j] + dp[i + 1][j][neu]);
-                    if (j + 1 < m)
-                        best = max(best, coins[i][j] + dp[i][j + 1][neu]);
-
-                 
-                    if (neu > 0 && coins[i][j] < 0) {
-                        if (i + 1 < n)
-                            best = max(best, dp[i + 1][j][neu - 1]);
-                        if (j + 1 < m)
-                            best = max(best, dp[i][j + 1][neu - 1]);
-                    }
-
-                    dp[i][j][neu] = best;
-                }
-            }
-            }
-      return dp[0][0][2];
+        int take=coins[i][j]+max(solve(i+1,j,neu,coins),solve(i,j+1,neu,coins));
+        int nottake=INT_MIN;
+        if(neu>0 && coins[i][j]<0){
+            nottake=max(solve(i+1,j,neu-1,coins),solve(i,j+1,neu-1,coins));
+        }
+        return dp[i][j][neu]=max(take,nottake);
+    }
+    int maximumAmount(vector<vector<int>>& coins) {
+           int n=coins.size();
+        int m=coins[0].size();
+        dp.resize(n+1, vector<vector<int>> (m+1, vector<int> (3,INT_MIN)));
+       return solve(0,0,2,coins);
     }
 };
