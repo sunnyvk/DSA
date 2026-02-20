31dffef8bf587341bcf679692298108faae30b7c
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-20 12:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (18.69%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,27 +1,32 @@
 class Solution {
-       int rob1(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,-1);
-        dp[0]=nums[0];
-        for(int ind=1;ind<n;ind++){
-             int pick=nums[ind];
-             if(ind>1)
-             pick+=dp[ind-2];
-          int notpick=0+dp[ind-1];
-          dp[ind]=max(pick,notpick); 
-        }
-       return dp[n-1];
-        
-    }
 public:
+   int solve(int ind, vector<int>& nums, vector<int>& dp){
+        if(ind == 0) return nums[0];
+        if(ind == 1) return max(nums[0], nums[1]);
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int take = nums[ind] + solve(ind-2, nums, dp);
+        int nottake = solve(ind-1, nums, dp);
+
+        return dp[ind] = max(take, nottake);
+    }
+int maxAdjacentSum(vector<int>& nums){
+     int n = nums.size();
+    if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        vector<int> dp(n, -1);
+        return solve(n-1, nums, dp);
+}
     int rob(vector<int>& nums) {
-        vector<int> temp1,temp2;
         int n=nums.size();
-        if(n==1) return nums[0];
+          if(n == 1) return nums[0];
+        vector<int> temp1,temp2;
         for(int i=0;i<n;i++){
             if(i!=0) temp1.push_back(nums[i]);
             if(i!=n-1) temp2.push_back(nums[i]);
         }
-        return max(rob1(temp1),rob1(temp2));
+        return max(maxAdjacentSum(temp1),maxAdjacentSum(temp2));
     }
 };
