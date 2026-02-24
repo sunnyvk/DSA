c213284c2f9e5e3c3988a06ae9945b5a06c3dfc6
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-24 11:55:05 +0530

    Time: 300 ms (39.15%) | Memory: 254.4 MB (49.71%) - LeetSync

--- 2717-collect-coins-in-a-tree/collect-coins-in-a-tree.cpp
+++ 2717-collect-coins-in-a-tree/collect-coins-in-a-tree.cpp
@@ -1,51 +1,53 @@
 class Solution {
-    public:
+public:
     int collectTheCoins(vector<int>& coins, vector<vector<int>>& edges) {
-
         int n=coins.size();
-        vector<set<int>> g(n);
-        
-        for(auto& e:edges)  g[e[0]].insert(e[1]),g[e[1]].insert(e[0]);
-        queue<int> leaves;
-
-        int totalEdges=edges.size()*2,deleted=0;
-        // let's add the leaves which don't have any coins!
-        
-        for(int i=0;i<n;i++)    if(g[i].size()==1 && coins[i]==0)  leaves.push(i);
-
-        while(leaves.size()){   // keep removing leaves with no coins!
-            int leaf=leaves.front();
-            leaves.pop();
-            if(g[leaf].empty()) continue;// this not has no parent! [last node of the tree]
-
-            int p=*g[leaf].begin();     // parent
-            
-            g[p].erase(leaf),deleted++;
-            g[leaf].erase(p),deleted++;        // clear the entire mappings for it as it's useless now!
-            
-            if(g[p].size()==1 && coins[p]==0)   leaves.push(p); // if this now becomes a leaf with no coins. 
+        vector<vector<int>> adj(n);
+        vector<int> deg(n,0);
+        for(auto edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            deg[u]++;
+            deg[v]++;
         }
-        // now the leaves would be having the coin! 
-        for(int i=0;i<n;i++)    if(g[i].size()==1)  leaves.push(i);
-        // now I need to remove the leaf nodes and their immediate parents
-        int step=2; //1 for parent and 1 for children
-        while(step--){
-            int sz=leaves.size();
-            while(sz--){
-                int leaf=leaves.front();
-                leaves.pop();
-                if(!g[leaf].size()) continue;// this not has no parent! [last node of the tree]
-                
-                int p=*g[leaf].begin();     // only parent would be there in leaf!
-                
-                g[p].erase(leaf),deleted++;
-                g[leaf].erase(p),deleted++;
 
-                if(g[p].size()==1)  leaves.push(p);
-            
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(coins[i]==0 && deg[i]==1) q.push(i);
+        }
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            if(deg[u]==0) continue;
+            deg[u]=0;
+            for(auto v:adj[u]){
+                if(deg[v]>0){
+                    deg[v]--;
+                    if(deg[v]==1 && coins[v]==0)
+                    q.push(v);
+                }
             }
         }
-        
-        return totalEdges-deleted;
+        for(int s=0;s<2;s++){
+           for(int i=0;i<n;i++){
+             if(deg[i]==1) q.push(i);
+           }
+            while(!q.empty()){
+               int u=q.front();
+                q.pop();
+                if(deg[u]==0) continue;
+                deg[u]=0; 
+                for(auto v:adj[u]){
+                    if(deg[v]>0) deg[v]--;
+                } 
+            }
+        }
+        int cnt=0;
+        for(auto e:edges){
+            if(deg[e[0]]>0 && deg[e[1]]>0 ) cnt++;
+        }
+        return cnt*2;
     }
 };
