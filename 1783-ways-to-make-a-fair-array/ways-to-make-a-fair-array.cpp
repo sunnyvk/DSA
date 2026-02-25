b4cf9a037f070ed1d49967a0d660020157cb0d0c
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-02-25 12:22:13 +0530

    Time: 3 ms (76.06%) | Memory: 98.1 MB (71.53%) - LeetSync

--- 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     int waysToMakeFair(vector<int>& nums) {
-        int res = 0;
-        int n =nums.size();
-        int righteven = 0, rightodd = 0;
-        
-        for(int i=0; i<n; ++i) {
-            if(i%2 == 0) righteven += nums[i];
-            else rightodd += nums[i];
+        int n=nums.size();
+        int osum=0,esum=0;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0) esum+=nums[i];
+            else osum+=nums[i];
         }
-        
-        int lefteven = 0, leftodd = 0;
-        for(int i=0; i<n; ++i) {
-            
-            if(i%2 == 0) {
-                righteven -= nums[i];
-            } else {
-                rightodd -= nums[i];
-            }
-            
-            if(lefteven + rightodd == leftodd + righteven)
-                ++res;
-            
-            if(i%2== 0) {
-                lefteven += nums[i];
-            } else {
-                leftodd += nums[i];
-            }
+         for(int i=0;i<n;i++){
+          if(i%2==0) esum-=nums[i];
+          else osum-=nums[i];
+
+          if(esum==osum) cnt++;
+          if(i%2==0) osum+=nums[i];
+          else esum+=nums[i];
         }
-        
-        return res;
+return cnt;
     }
 };
