3c7a8329c3841e751e163c5cbec3465910baebe0
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-09-18 19:51:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (43.59%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        int cur=0;
+        for(char c:s){
+            if(c=='('){
+                st.push(cur);
+                cur=0;
+            }else{
+                cur += st.top()+max(cur,1);
+                st.pop();
+            }
+        }
+        return cur;
+    }
+};
