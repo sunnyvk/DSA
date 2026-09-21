7dad3d8dde0dbd25bd2c53bb9d6fc00d3e4aeee4
Sunny Khambayat <115800794+sunnyvk@users.noreply.github.com>
2026-09-21 17:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (47.81%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char, int> mp;
+        int n = s.size();
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            if (mp.find(s[i]) != mp.end()) {
+                ans = max(ans, i - mp[s[i]] - 1);
+            } else {
+                mp[s[i]] = i;
+            }
+        }
+        return ans;
+    }
+};
